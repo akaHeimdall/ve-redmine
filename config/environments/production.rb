0f0aceaadd535707bdac45e838ccd3c9a1df8b54
Saeed Richardson <srichardson+mbp17@virtualevals.org>
2013-07-18 02:23:35 -0500

    * Trying to place mailer directly in production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,4 +30,15 @@ RedmineApp::Application.configure do
   config.action_mailer.logger = nil
 
   config.active_support.deprecation = :log
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
 end
